3c5e7205b2143504362e06035aa23a7952a9adb0
NhamNN <ngocnham14022002@gmail.com>
2025-08-18 14:39:10 +0700

    update

--- hadoop-ecommerce-ecosystem/docker/hadoop-base/scripts/format-namenode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Formatting Hadoop Namenode..."
-
-# Format the namenode
-$HADOOP_HOME/bin/hdfs namenode -format -force -nonInteractive
-
-echo "Namenode formatting completed."
--- hadoop-ecommerce-ecosystem/stop-demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Dừng Hadoop E-commerce Demo..."
-
-# Dừng tất cả services
-docker-compose -f docker-compose-demo.yml down
-
-echo "Demo đã được dừng."
